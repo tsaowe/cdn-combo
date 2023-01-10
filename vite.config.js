1b848f638a61e647bea3fc845d4f9bdebb20273c
caowei4465@haixue.com <caowei>
2023-01-10 13:45:12 +0800

    feat: main - console config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   root: "src",
   plugins: [react()],
   server: {
@@ -26,3 +26,5 @@ export default defineConfig({
     },
   }
 });
+console.log(config);
+export default config;
