c39cfb2298d33ad04a3b8503950572ffe7280d31
caowei4465@haixue.com <caowei>
2023-01-11 14:42:46 +0800

    feat: main - check container before rm

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,15 @@
 
 # origin command `docker run -d --name cdn-combo-container -p 9999:4000 cdn-combo-image`
 
-docker run -d --name cdn-combo-container -p 9999:4000 cdn-combo-image
+# check if container exists
+containerName="cdn-combo-container"
+containerId=`docker ps -a | grep $containerName | awk '{print $1}'`
+
+# if container exists, remove it
+if [ -n "$containerId" ]; then
+    docker rm -f $containerId
+fi
+
+docker run -d --name $containerName -p 9999:4000 cdn-combo-image
 
 echo "Deployed! port 9999, container name: cdn-combo-container"
